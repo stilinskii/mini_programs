e3b9626f5e949d62a6e06dfc18c9710b86e0aba9
nopainnomoney <lhy1031hy@gmail.com>
2022-03-22 14:57:41 +0900

    using class method

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 public class Calendar {
+	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getmaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("달을 입력해 주세요");
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		int monthdays = maxDays[month - 1];
 		
-		System.out.printf("%d월의 마지막일은 %d일입니다.",month,monthdays);
+		System.out.printf("%d월의 마지막일은 %d일입니다.\n",month,cal.getmaxDaysOfMonth(month));
 		scanner.close();
 		
 	
