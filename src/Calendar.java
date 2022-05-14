f37409e554485e10af22a1141657940305d57c64
stilinskii <lhy1031hy@gmail.com>
2022-05-14 23:50:35 +0900

    add printCalendar method

--- src/Calendar.java
+++ src/Calendar.java
@@ -4,14 +4,13 @@ public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getmaxDaysOfMonth(int month) {
+	public static int getmaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
 
 	public static void main(String[] args) {
 		String PROMPT = "cal> ";
 		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
 
 		while (true) {
 			System.out.println("월을 입력하세요");
@@ -19,17 +18,35 @@ public class Calendar {
 			int month = scanner.nextInt();
 			if (month == -1) {
 				break;
-			} else if (month > 12) {
+			} else if (month > 12 || month <= 0) {
 				continue;
 			} else {
-				System.out.printf("%d월의 마지막일은 %d일입니다.\n", month, cal.getmaxDaysOfMonth(month));
+				printCalendar(2022,month);
+				
 			}
-
 		}
 
-		System.out.println("끝");
+			System.out.println("끝");
 
-		scanner.close();
+			scanner.close();
 
+		
 	}
+	
+	public static void printCalendar(int year, int month) {
+		int maxDayOfMonth = getmaxDaysOfMonth(month);
+		System.out.printf("%d월의 마지막일은 %d일입니다.\n", month, maxDayOfMonth);
+		System.out.printf("     <%d년 %d월>\n",year, month);
+		System.out.println(" SU MO TU WE TU FR SA");
+		System.out.println("----------------------");
+		for (int i = 1; i <= maxDayOfMonth; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+		
+	}
+	
 }
